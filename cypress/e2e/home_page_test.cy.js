431b60fa6aa0864478ce2d94834604ac905ad0aa
Christopher Lim <toffer.lim87@gmail.com>
2022-08-22 08:12:12 -0700

    review/sc-25 - update the home page test

--- cypress/e2e/home_page_test.cy.js
+++ cypress/e2e/home_page_test.cy.js
@@ -1,8 +1,4 @@
-describe('C12 Home Page test', () => {
-    it('C12 User opens either Chrome or Firefox Browser', () => {
-        cy.wait(1000);
-    });
-
+describe('Home Page test', () => {
     it('C12 User navigates to www.example.com/home', () => {
         cy.wait(1000);
     });
