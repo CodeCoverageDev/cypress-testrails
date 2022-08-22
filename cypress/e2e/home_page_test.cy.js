4d2ab47dfdaf573041eb6f51232fb3b82edca47f
Christopher Lim <toffer.lim87@gmail.com>
2022-08-22 08:05:16 -0700

    review/sc-25 - add new test cases

---
+++ cypress/e2e/home_page_test.cy.js
@@ -0,0 +1,9 @@
+describe('C12 Home Page test', () => {
+    it('User opens either Chrome or Firefox Browser', () => {
+        cy.wait(1000);
+    });
+
+    it('User navigates to www.example.com/home', () => {
+        cy.wait(1000);
+    });
+});
