d90595dac9c0569b9fba1a44992f55f5a2fe7217
Christopher Lim <toffer.lim87@gmail.com>
2022-08-22 07:49:33 -0700

    review/sc-25 - Updating the Cypress Config and testing the CI job

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-testrail-simple/src/plugin')(on, config)
+      return require('./cypress/plugins/index.js')(on, config)
     },
   },
 });
