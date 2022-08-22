c5c34579fa5650043781c4b4f06201330df6d71f
Christopher Lim <toffer.lim87@gmail.com>
2022-08-22 08:36:41 -0700

    review/sc-25 - adding the c12 in another test block

--- cypress/e2e/home_page_test.cy.js
+++ cypress/e2e/home_page_test.cy.js
@@ -1,5 +1,9 @@
-describe('C12 Home Page test', () => {
-    it('Everyone can view the home page', () => {
+describe('Home Page test', () => {
+    it('C12 Testing', () => {
+        cy.wait(1000);
+    });
+
+    it('C12 Everyone can view the home page', () => {
         cy.wait(1000);
     });
 });
